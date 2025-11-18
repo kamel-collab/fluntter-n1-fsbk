c0d2dd62c80622a731c52077345ea1671f574dd2
Kamel Bahmed <kamel.bahmed.info@gmail.com>
2025-11-18 17:47:08 +0100

    done

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -12,7 +12,67 @@ class HomeScreen extends StatelessWidget {
         backgroundColor: Colors.deepOrange,
         elevation: 4.0,
       ),
-      body: Center(child: Text('Home Screen')),
+      body: Center(
+        child: InkWell(
+          onTap: () {},
+          child: Card(
+            elevation: 4.0,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(8.0),
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Stack(
+                  children: [
+                    AspectRatio(
+                      aspectRatio: 1,
+                      child: Image.network(
+                        "https://d2v5dzhdg4zhx3.cloudfront.net/web-assets/images/storypages/primary/ProductShowcasesampleimages/JPEG/Product+Showcase-1.jpg",
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                    Positioned(
+                      top: 8,
+                      right: 8,
+                      child: Container(
+                        decoration: BoxDecoration(
+                          color: Colors.black87,
+                          shape: BoxShape.circle,
+                        ),
+                        padding: EdgeInsets.all(6.0),
+                        child: Icon(Icons.favorite_border),
+                      ),
+                    ),
+                  ],
+                ),
+
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        "product title",
+                        style: TextStyle(fontWeight: FontWeight.bold),
+                      ),
+                      SizedBox(height: 4),
+                      Text(
+                        "21356 da",
+                        style: TextStyle(
+                          color: Colors.deepOrange,
+                          fontSize: 13,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
       bottomNavigationBar: BottomNavigationBar(
         currentIndex: 0,
         backgroundColor: Colors.deepOrange,
