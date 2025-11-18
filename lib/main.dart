5f8b334ea043771bd0d9c0809042c6c9c354fb8f
Kamel Bahmed <kamel.bahmed.info@gmail.com>
2025-11-18 09:56:44 +0100

    done

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,5 @@
-import 'package:first/screens/home_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(home: HomeScreen());
-  }
+  runApp();
 }
